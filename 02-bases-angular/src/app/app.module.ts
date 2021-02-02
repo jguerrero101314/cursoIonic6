ea190cf3dab621b12da11a73dccfddd39b7a1ee0
Joel Guerrero <joelg1014@hotmail.com>
2021-02-02 15:26:32 -0800

    23. Módulo de páginas

--- 02-bases-angular/src/app/app.module.ts
+++ 02-bases-angular/src/app/app.module.ts
@@ -2,23 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './components/menu/menu.component';
+import { PagesModuleModule } from './pages/pages-module.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent,
     MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
