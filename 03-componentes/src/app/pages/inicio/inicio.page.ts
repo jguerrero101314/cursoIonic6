c692ce60e55007782d4c03bd1b04601638f16ef0
Joel Guerrero <joelg1014@hotmail.com>
2021-02-20 17:40:51 -0800

    63. ion-menu

--- 03-componentes/src/app/pages/inicio/inicio.page.ts
+++ 03-componentes/src/app/pages/inicio/inicio.page.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Componente } from '../../interfaces/interfaces';
+import { DataService } from '../../service/data.service';
 
-interface Componente{
-  icon:string;
-  name:string;
-  redirecto:string;
-}
+
 
 @Component({
   selector: 'app-inicio',
@@ -14,86 +13,12 @@ interface Componente{
 export class InicioPage implements OnInit {
 
 
-  componentes: Componente[] = [
-    {
-      icon: 'american-football-outline',
-      name: 'Action Sheet',
-      redirecto: '/action-sheet'
-    },
-    {
-      icon: 'alert-circle-outline',
-      name: 'Alert',
-      redirecto: '/alert'
-    },
-    {
-      icon: 'beaker-outline',
-      name: 'avatar',
-      redirecto: '/avatar'
-    },
-    {
-      icon: 'cog-outline',
-      name: 'Tabs',
-      redirecto: '/tabs'
-    },
-    {
-      icon: 'radio-button-off-outline',
-      name: 'Buttons',
-      redirecto: '/button'
-    },
-    {
-      icon: 'card-outline',
-      name: 'Cards',
-      redirecto: '/card'
-    },
-    {
-      icon: 'checkmark-circle-outline',
-      name: 'Checks',
-      redirecto: '/check'
-    },
-    {
-      icon: 'calendar-outline',
-      name: 'Date Time',
-      redirecto: '/date-time'
-    },
-    {
-      icon: 'car-outline',
-      name: 'Fab',
-      redirecto: '/fab'
-    },
-    {
-      icon: 'grid-outline',
-      name: 'Grid',
-      redirecto: '/grid'
-    },
-    {
-      icon: 'infinite-outline',
-      name: 'Infinite',
-      redirecto: '/infinite'
-    },
-    {
-      icon: 'hammer-outline',
-      name: 'Input Forms',
-      redirecto: '/input'
-    },
-    {
-      icon: 'list-outline',
-      name: 'List-sliding',
-      redirecto: '/list'
-    },
-    {
-      icon: 'reorder-three-outline',
-      name: 'List - Reorder',
-      redirecto: '/list-reorder'
-    },
-    {
-      icon: 'refresh-circle-outline',
-      name: 'Loading',
-      redirecto: '/loading'
-    }
-  ];
-  constructor() { }
+  componentes:Observable<Componente[]>;
+  
+  constructor( private dataService:DataService) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
   }
 
 }
