6deeb4cf43c6d4f381551d097c8c4ac90d7e64ea
Joel Guerrero <joelg1014@hotmail.com>
2021-03-15 08:12:54 -0700

    probando

--- 04-noticias/src/app/app.module.ts
+++ 04-noticias/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+// import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,3 +23,5 @@ import { HttpClientModule } from '@angular/common/http';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+//InAppBrowser
